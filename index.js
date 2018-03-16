92107a4959019537500fac2f230f1151e0942455
bear gates <beargates@beardeMacBook-Air.local>
2018-03-16 16:13:34 +0800

    加入asyncStorage，react-native-storage

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
+import Storage from 'react-native-storage';
+import asyncStorageConfig from './src/common/config/asyncStorage';
 import App from './src/App';
+
+global.storage = new Storage(asyncStorageConfig);
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
